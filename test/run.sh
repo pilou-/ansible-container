7381da4fadbfa7c94596b27581597254ba61672e
Pierre-Louis Bonicoli <pierre-louis@libregerbil.fr>
2016-10-28 00:21:13 +0200

    Allow to run the test suite using multiple versions of Python

--- test/run.sh
+++ test/run.sh
@@ -9,11 +9,12 @@
 source_root=$(python -c "from os import path; print(path.abspath(path.join(path.dirname('$0'), '..')))")
 export ANSIBLE_CONTAINER_PATH=${source_root}
 
-image_exists=$(docker images local-test:latest | wc -l)
+export AC_PYTHON_VERSION=$(python -c "import platform; print('.'.join(platform.python_version_tuple()[0:2]))")
+image_exists=$(docker images local-test-"${AC_PYTHON_VERSION}":latest | wc -l)
 if [ "${image_exists}" -le 1 ]; then
    ansible-container --project "${source_root}/test/local" --debug build --local-builder --with-variables ANSIBLE_CONTAINER_PATH="${source_root}"
 fi
 
 ansible-container --project "${source_root}/test/local" --debug run
-status=$(docker inspect --format="{{ .State.ExitCode }}" ansible_test_1)
+status=$(docker inspect --format="{{ .State.ExitCode }}" ansible_test-"${AC_PYTHON_VERSION}"_1)
 exit "${status}"
