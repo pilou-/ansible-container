bc16f8d844fd6ff1bb45436955f8f4f030e94bb8
Pierre-Louis Bonicoli <pierre-louis@libregerbil.fr>
2018-02-08 10:52:49 +0100

    Fix TypeError exception (Python 3 compatibility)
    Error message:
    ______ TestAnsibleContainerConfig.test_should_also_replace_pwd_in_volumes ______
    
    self = <validate_config.TestAnsibleContainerConfig testMethod=test_should_also_replace_pwd_in_volumes>
    
        def test_should_also_replace_pwd_in_volumes(self):
            # test that ${PWD} gets resolved
            self.config.cli_vars_files = self.vars_files
            self.config.set_env('prod')
            container.ENV = 'conductor'
            container.utils.DataLoader = DataLoader
            container.utils.VariableManager = VariableManager
            container.utils.RoleInclude = RoleInclude
    >       conductor_config = AnsibleContainerConductorConfig(self.config._config)
    
    ansible-container/test/tests/validate_config.py:145:
    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
    ansible-container/container/__init__.py:19: in __wrapped__
        return fn(*args, **kwargs)
    ansible-container/container/config.py:357: in __init__
        self._process_defaults()
    ansible-container/container/config.py:390: in _process_defaults
        callback=lambda processed: self._templar.set_available_variables(
    ansible-container/container/config.py:375: in _process_section
        yaml.round_trip_dump(value, buffer)
    [...]
    venv/3.6/lib/python3.6/site-packages/ruamel/yaml/emitter.py:422: in expect_flow_sequence
        self.write_indicator(u' ' * ind + u'[', True, whitespace=True)
    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
    
    self = <ruamel.yaml.dumper.RoundTripDumper object at 0x7fbda6e59e48>
    indicator = '[', need_whitespace = True, whitespace = True, indention = False
    
        def write_indicator(self, indicator, need_whitespace,
                            whitespace=False, indention=False):
            # type: (Any, Any, bool, bool) -> None
            if self.whitespace or not need_whitespace:
                data = indicator
            else:
                data = u' ' + indicator
            self.whitespace = whitespace
            self.indention = self.indention and indention
            self.column += len(data)
            self.open_ended = False
            if bool(self.encoding):
                data = data.encode(self.encoding)
    >       self.stream.write(data)
    E       TypeError: a bytes-like object is required, not 'str'
    
    venv/3.6/lib/python3.6/site-packages/ruamel/yaml/emitter.py:1059: TypeError

--- container/config.py
+++ container/config.py
@@ -6,13 +6,12 @@ from .utils.visibility import getLogger
 logger = getLogger(__name__)
 
 from abc import ABCMeta, abstractproperty, abstractmethod
-from io import BytesIO
 import os
 from os import path
 import copy
 import json
 import re
-from six import add_metaclass, iteritems, PY2, string_types, text_type
+from six import add_metaclass, iteritems, PY2, string_types, text_type, StringIO
 
 from collections import Mapping
 from .utils.ordereddict import ordereddict
@@ -371,7 +370,7 @@ class AnsibleContainerConductorConfig(Mapping):
             elif isinstance(value, (list, dict)):
                 # if it's a dimensional structure, it's cheaper just to serialize
                 # it, treat it like a template, and then deserialize it again
-                buffer = BytesIO() # use bytes explicitly, not unicode
+                buffer = StringIO()
                 yaml.round_trip_dump(value, buffer)
                 processed[key] = yaml.round_trip_load(
                     templar.template(buffer.getvalue())
